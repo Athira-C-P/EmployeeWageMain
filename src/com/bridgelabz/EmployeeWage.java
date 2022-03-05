315dbae4d57c18c121d67f3ad06dc88bb5e4bab3
Athira-C-P <athiracp4422@gmail.com>
2022-03-06 02:26:21 +0530

    Added program Calculate Wages till a condition of total working hours or days is reached for a month

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,19 +1,20 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-
     static final int WAGE_PER_HOUR = 20;
-    static final int IS_FULL_TIME = 1;
-    static final int IS_PART_TIME = 2;
-
-
+    static final int IS_PART_TIME = 1;
+    static final int IS_FULL_TIME = 2;
+    static final int MAX_WORKING_DAYS = 20;
+    static final int MAX_WORKING_HRS = 100;
     public static void main(String[] args) {
-        System.out.println("welcome to Employee Wage Computation Program");
+        System.out.println("Welcome to Employee Wage Computation Program");
         int empHrs = 0;
         int empWage = 0;
         int totalEmpWage = 0;
         int totalWorkingDays = 0;
-        while (totalWorkingDays < 20) {
+        int totalWorkingHrs = 0;
+
+        while (totalWorkingDays < MAX_WORKING_DAYS && totalWorkingHrs <= MAX_WORKING_HRS) {
             int check = (int) Math.floor(Math.random() * 10) % 3;
             switch (check) {
                 case IS_PART_TIME:
@@ -27,8 +28,11 @@ public class EmployeeWage {
             }
             empWage = WAGE_PER_HOUR * empHrs;
             totalEmpWage = totalEmpWage + empWage;
+            totalWorkingHrs = totalWorkingHrs + empHrs;
             totalWorkingDays++;
         }
-        System.out.println("Employee wage is " + totalEmpWage);
+        System.out.println("Total Working Hours = " +totalWorkingHrs);
+        System.out.println("Total Working Days = " +totalWorkingDays);
+        System.out.println("Total Employee wage is " +totalEmpWage);
     }
 }
